52e69646c32d256c770c21e63663697e82db283b
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2022-06-27 19:15:53 +0200

    📦 NEW : Add versions API endpoints

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const hostDDRagon = "https://ddragon.leagueoflegends.com";
 app.use(express.json());
 
 /* # Endpoints # */
-/* ## All Versions ## */
+/* ## Versions ## */
+// All Versions
 app.get("/versions", async (request, response) => {
     await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
         response.status(200).json(res.data);
@@ -27,26 +28,15 @@ app.get("/versions", async (request, response) => {
         console.log(error);
     });
 });
-
-/* ## Summoner ## */
-app.get("/summoner", async (request, response) => {
-    await axios.get(hostRiotAPIv1 + "/cdn/" + + "/data/fr_FR/champion.json", config).then((res) => {
-        response.status(200).json(res.data);
+// Lastest Version
+app.get("/version/lastest", async (request, response) => {
+    await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
+        response.status(200).json(res.data[0]);
     }).catch((error) => {
         response.status(500);
         console.log(error);
     });
 });
-
-/* ## All Champions ## */
-// app.get("/champions", async (request, response) => {
-//     await axios.get(hostDDRagon + "/lol/summoner/v4/summoners/by-name/Sn0W3838?api_key=RGAPI-6082214f-5a9e-4dc9-a596-4f9fdef2eec5", config).then((res) => {
-//         response.status(200).json(res.data);
-//     }).catch((error) => {
-//         response.status(500);
-//         console.log(error);
-//     });
-// });
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Serveur à l'écoute");
