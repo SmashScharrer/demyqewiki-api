d114de0aad5397f90b7b6b29f0460c3e018854a3
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2022-06-27 19:31:54 +0200

    📦 NEW : Add summoner API endpoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* # Require Dependencies # */
-const express = require("express");
+const { express, response } = require("express");
 const app = express();
 const axios = require("axios");
 
@@ -19,7 +19,7 @@ app.use(express.json());
 
 /* # Endpoints # */
 /* ## Versions ## */
-// All Versions
+// GET : All Versions
 app.get("/versions", async (request, response) => {
     await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
         response.status(200).json(res.data);
@@ -28,13 +28,24 @@ app.get("/versions", async (request, response) => {
         console.log(error);
     });
 });
-// Lastest Version
+// GET : Lastest Version
 app.get("/version/lastest", async (request, response) => {
     await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
         response.status(200).json(res.data[0]);
     }).catch((error) => {
         response.status(500);
         console.log(error);
+    });
+});
+
+/* ## Summoner ## */
+// GET : Summoner by name
+app.get("/summoner", async (request, response) => {
+    await axios.get(hostRiotAPIv1 + "/lol/summoner/v4/summoners/by-name/Sn0W3838?api_key=" + process.env.RIOT_API_KEY, config).then((res) => {
+        response.status(200).json(res.data);
+    }).catch((error) => {
+        console.log(error);
+        response.status(500);
     });
 });
 
