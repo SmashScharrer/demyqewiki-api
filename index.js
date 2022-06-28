7fedd824072e36dfcec5b8a9f10b3bfede1f69b8
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2022-06-28 11:32:23 +0200

    🔧 FIX : Champion masteries bug API endpoints

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.get("/versions", async (request, response) => {
     await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
         response.status(200).json(res.data);
     }).catch((error) => {
+        console.log("Error : " + error.code);
         response.status(500);
-        console.log(error);
     });
 });
 // GET : Lastest Version
@@ -33,8 +33,8 @@ app.get("/version/lastest", async (request, response) => {
     await axios.get(hostDDRagon + "/api/versions.json", config).then((res) => {
         response.status(200).json(res.data[0]);
     }).catch((error) => {
+        console.log("Error : " + error.code);
         response.status(500);
-        console.log(error);
     });
 });
 
@@ -44,17 +44,27 @@ app.get("/summoner", async (request, response) => {
     await axios.get(hostRiotAPIv1 + "/lol/summoner/v4/summoners/by-name/Sn0W3838?api_key=" + process.env.RIOT_API_KEY, config).then((res) => {
         response.status(200).json(res.data);
     }).catch((error) => {
-        console.log(error);
+        console.log("Error : " + error.code);
         response.status(500);
     });
 });
 
-// GET : Champion masteries by summoner name
-app.get("/champion-masteries", async (request, response) => {
-    await axios.get(hostRiotAPIv1 + "/lol/champion-mastery/v4/champion-masteries/by-summoner/Sn0W3838?api_key=" + process.env.RIOT_API_KEY, config).then((res) => {
+/* ## Masteries ## */
+// GET : Champion masteries by summoner ID
+app.get("/champion-masteries/:summonerId", async (request, response) => {
+    await axios.get(hostRiotAPIv1 + "/lol/champion-mastery/v4/champion-masteries/by-summoner/" + request.params.summonerId + "?api_key=RGAPI-6082214f-5a9e-4dc9-a596-4f9fdef2eec5", config).then((res) => {
         response.status(200).json(res.data);
     }).catch((error) => {
-        console.log(error);
+        console.log("Error : " + error.code);
+        response.status(500);
+    });
+});
+// GET : Champion mastery by summoner ID & champion ID
+app.get("/champion-masteries/:summonerId/by-champion/:championId", async (request, response) => {
+    await axios.get(hostRiotAPIv1 + "/lol/champion-mastery/v4/champion-masteries/by-summoner/" + request.params.summonerId + "/by-champion/" + request.params.championId + "?api_key=RGAPI-6082214f-5a9e-4dc9-a596-4f9fdef2eec5", config).then((res) => {
+        response.status(200).json(res.data);
+    }).catch((error) => {
+        console.log("Error : " + error.code);
         response.status(500);
     });
 });
