0e9b9d908b3090ef5d3c96dc284e595348b6c4f6
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2022-06-28 12:26:34 +0200

    📦 NEW : Add summoner spells, runes reforged & items API endpoints

--- index.js
+++ index.js
@@ -71,6 +71,44 @@ app.get("/champion-masteries/:summonerName/by-champion/:championId", async (requ
     });
 });
 
+/* ## Summoner Spells ## */
+// GET : All summoner spells by lastest version
+app.get("/summoner-spells", async (request, response) => {
+    const version = await axios.get(hostDDRagon + "/api/versions.json", config);
+    await axios.get(hostDDRagon + "/cdn/" + version.data[0] + "/data/fr_FR/summoner.json", config).then((res) => {
+        response.status(200).json(res.data);
+    }).catch((error) => {
+        console.log("Error : " + error.code);
+        response.status(500);
+    });
+});
+
+/* ## Runes Reforged ## */
+// GET : All runes reforged by lastest version
+app.get("/runes", async (request, response) => {
+    const version = await axios.get(hostDDRagon + "/api/versions.json", config);
+    await axios.get(hostDDRagon + "/cdn/" + version.data[0] + "/data/fr_FR/runesReforged.json", config).then((res) => {
+        response.status(200).json(res.data);
+    }).catch((error) => {
+        console.log("Error : " + error.code);
+        response.status(500);
+    });
+});
+
+/* ## Items ## */
+// GET : All items by lastest version
+app.get("/items", async (request, response) => {
+    const version = await axios.get(hostDDRagon + "/api/versions.json", config);
+    await axios.get(hostDDRagon + "/cdn/" + version.data[0] + "/data/fr_FR/item.json", config).then((res) => {
+        response.status(200).json(res.data);
+    }).catch((error) => {
+        console.log("Error : " + error.code);
+        response.status(500);
+    });
+});
+
+/* ## Matchs ## */
+
 app.listen(process.env.PORT || 5000, () => {
     console.log("Serveur à l'écoute");
 });
